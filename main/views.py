83ce95f355c314d314b1e3ec812352ac68ce6e83
muttyqt15 <hayaye69@gmail.com>
2024-09-18 13:37:34 +0700

    shboom

--- main/views.py
+++ main/views.py
@@ -3,27 +3,71 @@ from main.forms import MoodEntryForm
 from main.models import MoodEntry
 from django.http import HttpResponse
 from django.core import serializers
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+import datetime
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 # Create your views here.
+
+def login_user(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            response = HttpResponseRedirect(reverse("main:show_main"))
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
+    else:
+        form = AuthenticationForm(request)
+    context = {'form': form}
+    return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    return response
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+@login_required(login_url='main:login')
 def show_main(request):
     mood_entries = MoodEntry.objects.all()
-
+    print(request.COOKIES['last_login'])
     context = {
-        'name': 'Pak Bepe',
-        'class': 'PBP D',
-        'npm': '2306123456',
-        'mood_entries': mood_entries
-    }
+    'name': request.user.username,
+    'class': 'PBP A',
+    'npm': '2306207101',
+    'mood_entries': mood_entries,
+    'last_login': request.COOKIES['last_login'],
+}
 
     return render(request, "main.html", context)
 
 def create_mood_entry(request):
     form = MoodEntryForm(request.POST or None)
-    
+
     if form.is_valid() and request.method == "POST":
-        form.save()
-        return redirect('main:show_main') # Menggunakan nama di urls.py
-    
-    context = { "form" : form }
+        mood_entry = form.save(commit=False) # kenapa?
+        mood_entry.user = request.user
+        mood_entry.save()
+        return redirect('main:show_main')
+
+    context = {'form': form}
     return render(request, "create_mood_entry.html", context)
 
 def show_xml(request):
