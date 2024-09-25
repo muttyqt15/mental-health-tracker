ce42799aa1752928789120a77b1c5013bfc5cb62
muttyqt15 <hayaye69@gmail.com>
2024-09-25 13:40:06 +0700

    tutorial css

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -50,6 +50,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
+
 ]
 
 ROOT_URLCONF = 'mental_health_tracker.urls'
